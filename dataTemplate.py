6ee7df6e126d8e94c8b5fe77d722844eaf6b7455
Reed Schubert <101429468+reed-schubert@users.noreply.github.com>
2025-02-19 11:52:02 -0600

    Added the scripts for the data transformation

---
+++ dataTemplate.py
@@ -0,0 +1,18 @@
+#Compares the shape between an nii file and the AAL3 template
+import nibabel as nib
+import numpy as np
+
+
+# Load your fMRI data
+fmri_path = "/Users/reedschubert/Desktop/Comps/NlfTI/ADNI4_GE_UHP_20230131_13.nii"
+fmri_img = nib.load(fmri_path)
+fmri_data = fmri_img.get_fdata()
+
+# Load the AAL3 atlas
+aal_path = "/Users/reedschubert/Desktop/Comps/AAL3/AAL3v1.nii" 
+aal_img = nib.load(aal_path)
+aal_data = aal_img.get_fdata()
+
+# Check that the fMRI data and atlas have the same dimensions
+print("fMRI shape:", fmri_data.shape)
+print("AAL3 shape:", aal_data.shape)
---
+++ niireader.py
@@ -0,0 +1,11 @@
+#Gets the shape and time dimension of an nii file
+
+import nibabel as nib
+
+# Load the NIfTI file
+nii_file_path = '/Users/reedschubert/Desktop/Comps/NLNII/Patient1/Patient1.nii'
+img = nib.load(nii_file_path)
+
+# Get the image data (it will be a numpy array)
+data = img.get_fdata()
+print(data.shape)  # Check the shape of the data
